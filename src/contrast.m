cf2ff9b740c1d3ac0c0128add53067a040fea732
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-02-13 03:33:15 +0700

    add comments

--- src/contrast.m
+++ src/contrast.m
@@ -1,21 +1,36 @@
 function [imgOut] = contrast(fileName)
+
+    % read image
     imgIn = imread(fileName);
 
+    % image size
     [row, col, depth] = size(imgIn);
+
+    % output initialization
     imgOut = imgIn;
 
+    % show input image
     figure,imshow(imgIn);
+
+    % for each color
     for i=1:depth
         imgArr = imgIn(:,:,i);
+
+        % rmin & rmax
         rmin = min(imgArr(:));
         rmax = max(imgArr(:));
         
         % skip step set r<rmin -> 0, r>rmax -> 255
         % asumsi rmin = min(I) dan rmax = max(I)
+
+        % transform image
         imgOut(:,:,i) = (imgArr-rmin).*(255/(rmax-rmin)); 
     
+        % show histogram
         figure,image_histogram(imgArr);
         figure,image_histogram(imgOut(:,:,i));
     end
+
+    % show output color
     figure,imshow(imgOut);
 end
--- src/histogram_equalization.m
+++ src/histogram_equalization.m
@@ -1,43 +1,52 @@
 function [imgOut] = histogram_equalization(fileName)
 %HISTOGRAM_EQUALIZATION Summary of this function goes here
 %   Detailed explanation goes here
+
+    % read image
     imgIn = imread(fileName);
+
+    % output initialization
     imgOut = imgIn;
+    %hix = imgIn;
+
+    % image size
     [row, col, depth] = size(imgIn);
     n = row * col;
     disp(size(imgIn));
-
     
-    for i = 1:depth
-        histEq = zeros(256,1);
-        imgArr = imgIn(:,:,i);
+    % for each color
+    for d = 1:depth
+        imgArr = imgIn(:,:,d);
         hist = image_histogram(imgArr);
         %disp(hist)
         %disp(hist(200));
 
-        %figure,image_histogram(imgArr);
+        % create T(rk) 
+        histFunc = zeros(256,1);
 
-        for j = 1:256 
+        % calculate probability for each rk
+        for i = 1:256 
             sum = 0;
-            for k = 1:j
-                sum = sum + hist(k)/n;
+            for j = 1:i
+                sum = sum + hist(j)/n;
             end
-            histEq(j) = floor(sum*255);
+            histFunc(i) = floor(sum*255);
         end
-
-        for j = 1:row
-            for k = 1:col
-                %disp(imgIn(j,k,i));
-                imgOut(j,k,i) = histEq(imgArr(j,k)+1);
+        
+        % transform image based on T(rk)
+        for i = 1:row
+            for j = 1:col
+                imgOut(i,j,d) = histFunc(imgArr(i,j)+1);
             end
         end
-        disp(max(imgArr(:)));
-        disp(min(imgArr(:)));
-
-        figure,bar(hist);
-        figure,bar(histEq);
-        figure,image_histogram(imgOut(:,:,i));
+        %disp(max(imgArr(:)));
+        %disp(min(imgArr(:)));
+            %figure,bar(histFunc);
+        figure,image_histogram(imgOut(:,:,d));
         figure,image_histogram(histeq(imgArr));
-
+        %hix(:,:,d) = histeq(imgArr);
     end
+    figure,imshow(imgIn);
+    figure,imshow(imgOut);
+    %figure,imshow(hix);
 end
