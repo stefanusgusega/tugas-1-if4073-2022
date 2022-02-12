75fd53117a5a792d5eb08a7ff14e2eec0cd244b0
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-02-13 02:54:26 +0700

    add RGB case for contrast & histogram equalization

--- src/contrast.m
+++ src/contrast.m
@@ -1,19 +1,21 @@
 function [imgOut] = contrast(fileName)
     imgIn = imread(fileName);
 
-    rmin = min(imgIn(:));
-    rmax = max(imgIn(:));
-    
-    % skip step set r<rmin -> 0, r>rmax -> 255
-    % asumsi rmin = min(I) dan rmax = max(I)
-
-    imgOut = (imgIn-rmin).*(255/(rmax-rmin)); 
+    [row, col, depth] = size(imgIn);
+    imgOut = imgIn;
 
     figure,imshow(imgIn);
-    figure,image_histogram(imgIn);
-    figure,imshow(imgOut);
-    figure,image_histogram(imgOut);
-end
-
-
+    for i=1:depth
+        imgArr = imgIn(:,:,i);
+        rmin = min(imgArr(:));
+        rmax = max(imgArr(:));
+        
+        % skip step set r<rmin -> 0, r>rmax -> 255
+        % asumsi rmin = min(I) dan rmax = max(I)
+        imgOut(:,:,i) = (imgArr-rmin).*(255/(rmax-rmin)); 
     
+        figure,image_histogram(imgArr);
+        figure,image_histogram(imgOut(:,:,i));
+    end
+    figure,imshow(imgOut);
+end
