3d4ca26684364f0797ee3176bb6ce4e84b1eedf0
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-02-13 00:30:11 +0700

    add contrast func

--- src/contrast.m
+++ src/contrast.m
@@ -1,13 +1,10 @@
-fileName = 'bean.jpg';
+function [imgOut] = contrast(fileName)
+    imgIn = imread(fileName);
 
-imgIn = imread(fileName);
-[imgOut] = contrast_func(imgIn);
-
-function [imgOut] = contrast_func(imgIn)
     rmin = min(imgIn(:));
     rmax = max(imgIn(:));
     
-    % step: set r<rmin -> 0, r>rmax -> 255. 
+    % step: set r<rmin -> 0, r>rmax -> 255
     % asumsi rmin = min(I) dan rmax = max(I)
 
     imgOut = (imgIn-rmin).*(255/(rmax-rmin)); % for all pixels
