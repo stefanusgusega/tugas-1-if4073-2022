38ea045cd0c7ede205150bea9a0072aea86e98f8
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-02-13 14:41:38 +0700

    fix: remove color case in functions

--- src/contrast.m
+++ src/contrast.m
@@ -4,32 +4,32 @@ function [imgOut] = contrast(fileName)
     imgIn = imread(fileName);
 
     % image size
-    [row, col, depth] = size(imgIn);
+    % [row, col, depth] = size(imgIn);
 
     % output initialization
-    imgOut = imgIn;
+    % imgOut = imgIn;
 
     % show input image
     figure,imshow(imgIn);
 
     % for each color
-    for i=1:depth
-        imgArr = imgIn(:,:,i);
+    % for i=1:depth
+    %imgArr = imgIn(:,:,i);
 
-        % rmin & rmax
-        rmin = min(imgArr(:));
-        rmax = max(imgArr(:));
-        
-        % skip step set r<rmin -> 0, r>rmax -> 255
-        % asumsi rmin = min(I) dan rmax = max(I)
-
-        % transform image
-        imgOut(:,:,i) = (imgArr-rmin).*(255/(rmax-rmin)); 
+    % rmin & rmax
+    rmin = min(imgIn(:));
+    rmax = max(imgIn(:));
     
-        % show histogram
-        figure,image_histogram(imgArr);
-        figure,image_histogram(imgOut(:,:,i));
-    end
+    % skip step set r<rmin -> 0, r>rmax -> 255
+    % asumsi rmin = min(I) dan rmax = max(I)
+
+    % transform image
+    imgOut = (imgIn-rmin).*(255/(rmax-rmin)); 
+
+    % show histogram
+    figure,image_histogram(imgIn);
+    figure,image_histogram(imgOut);
+    %end
 
     % show output color
     figure,imshow(imgOut);
--- src/histogram_equalization.m
+++ src/histogram_equalization.m
@@ -15,28 +15,30 @@ function [imgOut] = histogram_equalization(fileName)
     disp(size(imgIn));
     
     % for each color
-    for d = 1:depth
-        imgArr = imgIn(:,:,d);
-        histogram = image_histogram(imgArr);
-        %disp(hist)
-        %disp(hist(200));
+    % for d = 1:depth
+    
+    %imgArr = imgIn(:,:,d);
+    histogram = image_histogram(imgIn);
+    %disp(hist)
+    %disp(hist(200));
 
-        % create T(rk) 
-        histFunc = transform(histogram, n)
-        
-        % transform image based on T(rk)
-        for i = 1:row
-            for j = 1:col
-                imgOut(i,j,d) = histFunc(imgArr(i,j)+1);
-            end
+    % create T(rk) 
+    histFunc = transform(histogram, n);
+    
+    % transform image based on T(rk)
+    for i = 1:row
+        for j = 1:col
+            imgOut(i,j) = histFunc(imgIn(i,j)+1);
         end
-        %disp(max(imgArr(:)));
-        %disp(min(imgArr(:)));
-            %figure,bar(histFunc);
-        figure,image_histogram(imgOut(:,:,d));
-        figure,image_histogram(histeq(imgArr));
-        %hix(:,:,d) = histeq(imgArr);
     end
+
+    %disp(max(imgArr(:)));
+    %disp(min(imgArr(:)));
+
+    figure,image_histogram(imgOut);
+    figure,image_histogram(histeq(imgIn));
+    %hix(:,:,d) = histeq(imgArr);
+    %end
     figure,imshow(imgIn);
     figure,imshow(imgOut);
     %figure,imshow(hix);
